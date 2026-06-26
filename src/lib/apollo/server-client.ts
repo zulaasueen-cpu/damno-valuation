94281a469442fb55fbe5b6db8505658677b7b9e4
zulaasueen-cpu <zulaasueen-cpu@users.noreply.github.com>
2026-06-26 15:02:07 +0800

    fix: revert server client to original starter version

--- src/lib/apollo/server-client.ts
+++ src/lib/apollo/server-client.ts
@@ -1,21 +1,14 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-const HARDCODED_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6ImtaMDRwYVlSNmh0dXdKWENHRWVjOCIsImlhdCI6MTc4MjIxNTM5Mn0.LBd4utfh3xzpTFqi1s64jn1loW3W_zIXuBAffgaKrZM";
-
 export async function getServerApolloClient() {
-  const uri =
-    process.env.GRAPHQL_URL ??
-    process.env.NEXT_PUBLIC_GRAPHQL_URL ??
-    process.env.ERXES_ENDPOINT ??
-    process.env.NEXT_PUBLIC_ERXES_ENDPOINT ??
-    "/graphql";
-
   return new ApolloClient({
     link: new HttpLink({
-      uri,
+      uri:
+        process.env.GRAPHQL_URL ??
+        process.env.NEXT_PUBLIC_GRAPHQL_URL ??
+        "/graphql",
       headers: {
-        "x-app-token": HARDCODED_TOKEN,
+        "x-app-token": process.env.ERXES_APP_TOKEN ?? "",
       },
       fetchOptions: { cache: "no-store" },
     }),
